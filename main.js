9552678243d227557bddb02e3bab44f58d314375
adrianUrbS <121135128+adrianUrbS@users.noreply.github.com>
2023-08-29 15:13:56 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function setup() {
+    canvas = createCanvas(640, 480);
+    background("white")
+    video = createCapture(VIDEO);
+    video.size(640, 480)
+    video.hide()
+    frameRate(5)
+}
+
+function draw() {
+    image(video, 0, 0, 640, 480);
+    analizar();
+}
+function preload() {
+    ia = ml5.imageClassifier("mobileNet")
+}
+function analizar() {
+    ia.classify(canvas, respuesta)
+}
+function respuesta(error, resultados) {
+    if (!error) {
+        console.log(resultados);
+        dibujo = resultados[0].label;
+        confianza = Math.round(resultados[0].confidence * 100);
+        if (confianza > 40) {
+            document.getElementById("confianza").innerHTML = confianza + "%";
+            fetch("https://api.mymemory.translated.net/get?q=" + dibujo + "&langpair=en|es")
+                .then(response => response.json())
+                .then(data => {
+                    traduccion = data.responseData.translatedText;
+                    document.getElementById("dibujo").innerHTML = traduccion;
+                });
+        }
+    }
+}
